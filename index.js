9e09a60b3b31d5c61fc8fe20f7fdae2450d4b993
Ojas Mittal <ojasfarm31@gmail.com>
2023-10-01 10:48:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+var auth=false;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPass(req,res,next){
+    const password=req.body["password"];
+    if(password=="ILoveProgramming"){
+        auth=true;
+    }
+    next();
+}
+app.use(checkPass);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+  
+app.post("/check", (req, res) => {
+    if (auth) {
+      res.sendFile(__dirname + "/public/secret.html");
+    } else {
+      res.sendFile(__dirname + "/public/index.html");
+    }
+  });
+  
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
